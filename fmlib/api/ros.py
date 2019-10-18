1993225133862e443685f3ee8884d46abdbbaa3a
Argentina Ortega Sáinz <argenos@users.noreply.github.com>
2019-10-18 12:34:15 +0200

    api: Remove dependency to ropod_ros_msgs
    Closes #2

--- fmlib/api/ros.py
+++ fmlib/api/ros.py
@@ -5,8 +5,6 @@ import rospy
 
 from rospy_message_converter import message_converter
 
-from ropod_ros_msgs.msg import Task, TaskRequest
-
 
 class ROSInterface:
     """
@@ -24,7 +22,6 @@ class ROSInterface:
         self.logger.info("Initialized fleet management ROS interface")
 
     def _configure(self, **kwargs):
-        self.logger.error(kwargs)
         for publisher in kwargs.get('publishers', list()):
             pub = self.add_publisher(**publisher)
             self._publisher_dict[publisher.get('topic')] = pub
@@ -46,12 +43,6 @@ class ROSInterface:
         msg = self._get_ros_msg(msg_type, msg_module)
         callback = getattr(self, callback)
         return rospy.Subscriber(topic, msg, callback)
-
-    def task_cb(self, msg):
-        msg_dict = message_converter.convert_ros_message_to_dictionary(msg)
-        msg_ros = message_converter.convert_dictionary_to_ros_message('ropod_ros_msgs/TaskRequest', msg_dict)
-        task = Task()
-        self._publisher_dict['/fms/task'].publish(task)
 
     def start(self):
         rospy.loginfo("Started ROS interface of rospy")
