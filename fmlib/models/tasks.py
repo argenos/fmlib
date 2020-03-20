c84cd764b6e194ea352000c14b6e4a2db3e301fd
Ángela Enríquez Gómez <anenriqu@outlook.com>
2020-03-20 13:23:57 +0100

    tasks: Add TemporalConstraints to TaskConstraints
    TemporalConstraints include timepoint_constraints and inter_timepoint_constraints

--- fmlib/models/tasks.py
+++ fmlib/models/tasks.py
@@ -1,18 +1,21 @@
 import logging
 import uuid
+from datetime import datetime
 
-from fmlib.models.actions import Action, ActionProgress
-from fmlib.models.requests import TaskRequest
-from fmlib.utils.messages import Document
-from fmlib.utils.messages import Message
+import dateutil.parser
 from pymodm import EmbeddedMongoModel, fields, MongoModel
 from pymodm.context_managers import switch_collection
+from pymodm.errors import DoesNotExist
 from pymodm.manager import Manager
 from pymodm.queryset import QuerySet
 from pymongo.errors import ServerSelectionTimeoutError
 from ropod.structs.status import ActionStatus, TaskStatus as TaskStatusConst
 from ropod.utils.timestamp import TimeStamp
-from pymodm.errors import DoesNotExist
+
+from fmlib.models.actions import Action, ActionProgress
+from fmlib.models.requests import TaskRequest
+from fmlib.utils.messages import Document
+from fmlib.utils.messages import Message
 
 
 class TaskQuerySet(QuerySet):
@@ -69,60 +72,99 @@ TaskManager = Manager.from_queryset(TaskQuerySet)
 TaskStatusManager = Manager.from_queryset(TaskStatusQuerySet)
 
 
-class TimepointConstraints(EmbeddedMongoModel):
+class TimepointConstraint(EmbeddedMongoModel):
+    name = fields.CharField(primary_key=True)
     earliest_time = fields.DateTimeField()
     latest_time = fields.DateTimeField()
 
-    @staticmethod
-    def relative_to_ztp(timepoint, ztp, resolution="minutes"):
-        """ Returns the timepoint constraints relative to a ZTP (zero timepoint)
-
-        Args:
-            timepoint (TimepointConstraints): timepoint
-            ztp (TimeStamp): Zero Time Point. Origin time to which the timepoint will be referenced to
-            resolution (str): Resolution of the difference between the timepoint constraints and the ztp
-
-        Return: r_earliest_time (float): earliest time relative to the ztp
-                r_latest_time (float): latest time relative to the ztp
-        """
-
-        r_earliest_time = TimeStamp.from_datetime(timepoint.earliest_time).get_difference(ztp, resolution)
-        r_latest_time = TimeStamp.from_datetime(timepoint.latest_time).get_difference(ztp, resolution)
-
-        return r_earliest_time, r_latest_time
+    def __str__(self):
+        to_print = ""
+        to_print += "{}: [{}, {}]".format(self.name, self.earliest_time.isoformat(), self.latest_time.isoformat())
+        return to_print
 
     @classmethod
     def from_payload(cls, payload):
         document = Document.from_payload(payload)
-        timepoint_constraints = TimepointConstraints.from_document(document)
-        return timepoint_constraints
+        document["_id"] = document.pop("name")
+        document["earliest_time"] = dateutil.parser.parse(document.pop("earliest_time"))
+        document["latest_time"] = dateutil.parser.parse(document.pop("latest_time"))
+        return cls.from_document(document)
 
     def to_dict(self):
         dict_repr = self.to_son().to_dict()
         dict_repr.pop('_cls')
+        dict_repr["name"] = dict_repr.pop('_id')
         dict_repr["earliest_time"] = self.earliest_time.isoformat()
         dict_repr["latest_time"] = self.latest_time.isoformat()
         return dict_repr
 
 
-class TaskConstraints(EmbeddedMongoModel):
-    hard = fields.BooleanField(default=True)
-    timepoint_constraints = fields.EmbeddedDocumentListField(TimepointConstraints)
+class InterTimepointConstraint(EmbeddedMongoModel):
+    name = fields.CharField(primary_key=True)
+    mean = fields.FloatField()
+    variance = fields.FloatField()
+
+    @property
+    def standard_dev(self):
+        return round(self.variance ** 0.5, 3)
+
+    def __str__(self):
+        to_print = ""
+        to_print += "{}: N({}, {})".format(self.name, self.mean, self.standard_dev)
+        return to_print
 
     @classmethod
     def from_payload(cls, payload):
         document = Document.from_payload(payload)
-        timepoint_constraints = [TimepointConstraints.from_payload(timepoint_constraint)
-                                 for timepoint_constraint in document.get("timepoint_constraints")]
-        document["timepoint_constraints"] = timepoint_constraints
+        document["_id"] = document.pop("name")
+        return cls.from_document(document)
+
+    def to_dict(self):
+        dict_repr = self.to_son().to_dict()
+        dict_repr.pop('_cls')
+        dict_repr["name"] = dict_repr.pop('_id')
+        return dict_repr
+
+
+class TemporalConstraints(EmbeddedMongoModel):
+    timepoint_constraints = fields.EmbeddedDocumentListField(TimepointConstraint)
+    inter_timepoint_constraints = fields.EmbeddedDocumentListField(InterTimepointConstraint)
+
+    @classmethod
+    def from_payload(cls, payload):
+        document = Document.from_payload(payload)
+        document['timepoint_constraints'] = [TimepointConstraint.from_payload(timepoint_constraint)
+                                             for timepoint_constraint in document.get("timepoint_constraints")]
+        document['inter_timepoint_constraints'] = [InterTimepointConstraint.from_payload(inter_timepoint_constraint)
+                                                   for inter_timepoint_constraint in document.get("inter_timepoint_constraints")]
+        temporal_constraints = TemporalConstraints.from_document(document)
+        return temporal_constraints
+
+    def to_dict(self):
+        dict_repr = self.to_son().to_dict()
+        dict_repr.pop('_cls')
+        dict_repr['timepoint_constraints'] = [timepoint_constraint.to_dict()
+                                              for timepoint_constraint in self.timepoint_constraints]
+        dict_repr['inter_timepoint_constraints'] = [inter_timepoint_constraint.to_dict()
+                                                    for inter_timepoint_constraint in self.inter_timepoint_constraints]
+        return dict_repr
+
+
+class TaskConstraints(EmbeddedMongoModel):
+    hard = fields.BooleanField(default=True)
+    temporal = fields.EmbeddedDocumentField(TemporalConstraints)
+
+    @classmethod
+    def from_payload(cls, payload):
+        document = Document.from_payload(payload)
+        document["temporal"] = TemporalConstraints.from_payload(document.pop("temporal"))
         task_constraints = TaskConstraints.from_document(document)
         return task_constraints
 
     def to_dict(self):
         dict_repr = self.to_son().to_dict()
         dict_repr.pop('_cls')
-        timepoint_constraints = [timepoint_constraint.to_dict() for timepoint_constraint in self.timepoint_constraints]
-        dict_repr["timepoint_constraints"] = timepoint_constraints
+        dict_repr['temporal'] = self.temporal.to_dict()
         return dict_repr
 
 
@@ -140,6 +182,7 @@ class Task(MongoModel):
     duration = fields.FloatField()
     start_time = fields.DateTimeField()
     finish_time = fields.DateTimeField()
+    frozen = fields.BooleanField(default=False)
 
     objects = TaskManager()
 
@@ -167,18 +210,23 @@ class Task(MongoModel):
         return task
 
     @classmethod
-    def from_payload(cls, payload):
+    def from_payload(cls, payload, **kwargs):
         document = Document.from_payload(payload)
         document['_id'] = document.pop('task_id')
-        task = Task.from_document(document)
+        for key, value in kwargs.items():
+            document[key] = value.from_payload(document.pop(key))
+        task = cls.from_document(document)
         task.save()
         task.update_status(TaskStatusConst.UNALLOCATED)
         return task
 
-    def to_dict(self):
+    def to_dict(self, **kwargs):
         dict_repr = self.to_son().to_dict()
         dict_repr.pop('_cls')
         dict_repr["task_id"] = str(dict_repr.pop('_id'))
+        dict_repr["constraints"] = self.constraints.to_dict()
+        if kwargs.get("request"):
+            dict_repr["request"] = self.request.to_dict()
         return dict_repr
 
     def to_msg(self):
@@ -190,6 +238,28 @@ class Task(MongoModel):
         constraints = TaskConstraints(hard=request.hard_constraints)
         task = cls.create_new(request=request.request_id, constraints=constraints)
         return task
+
+    def freeze(self):
+        self.frozen = True
+        self.save()
+
+    def unfreeze(self):
+        self.frozen = False
+        self.save()
+
+    def mark_as_delayed(self):
+        task_status = Task.get_task_status(self.task_id)
+        task_status.delayed = True
+        task_status.save()
+
+    def unmark_as_delayed(self):
+        task_status = Task.get_task_status(self.task_id)
+        task_status.delayed = False
+        task_status.save()
+
+    def set_soft_constraints(self):
+        self.constraints.hard = False
+        self.save()
 
     def update_duration(self, duration):
         self.duration = duration
@@ -239,6 +309,53 @@ class Task(MongoModel):
             return True
         else:
             return False
+
+    def get_timepoint_constraint(self, name):
+        for constraint in self.constraints.temporal.timepoint_constraints:
+            if constraint.name == name:
+                return constraint
+
+    def get_inter_timepoint_constraint(self, name):
+        for constraint in self.constraints.temporal.inter_timepoint_constraints:
+            if constraint.name == name:
+                return constraint
+
+    def update_timepoint_constraint(self, name, earliest_time, latest_time=float('inf')):
+        in_list = False
+        for constraint in self.constraints.temporal.timepoint_constraints:
+            if constraint.name == name:
+                in_list = True
+                constraint.earliest_time = earliest_time
+                constraint.latest_time = latest_time
+        if not in_list:
+            self.constraints.temporal.timepoint_constraints.append(TimepointConstraint(name=name,
+                                                                                       earliest_time=earliest_time,
+                                                                                       latest_time=latest_time))
+        self.save()
+
+    def update_inter_timepoint_constraint(self, name, mean, variance):
+        in_list = False
+        for constraint in self.constraints.temporal.inter_timepoint_constraints:
+            if constraint.name == name:
+                in_list = True
+                constraint.mean = mean
+                constraint.variance = variance
+        if not in_list:
+            self.constraints.temporal.inter_timepoint_constraints.append(InterTimepointConstraint(name=name,
+                                                                                                  mean=mean,
+                                                                                                  variance=variance))
+        self.save()
+
+    @staticmethod
+    def get_earliest_task(tasks):
+        earliest_time = datetime.max
+        earliest_task = None
+        for task in tasks:
+            for constraint in task.constraints.temporal.timepoint_constraints:
+                if constraint.earliest_time < earliest_time:
+                    earliest_time = constraint.earliest_time
+                    earliest_task = task
+        return earliest_task
 
     @property
     def meta_model(self):
